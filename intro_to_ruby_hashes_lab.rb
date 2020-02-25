0a1ba593ea6d699c4be3962be331fcde2fd18d08
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 03:53:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,6 @@ def id_generator
   
 end
 
-###
-
 def my_hash_creator(key, value)
 
   { 
@@ -45,8 +43,6 @@ def my_hash_creator(key, value)
   }
   
 end
-
-###
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
